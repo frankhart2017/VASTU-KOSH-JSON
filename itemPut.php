a530a5ab9487b31d29a2ab45103aed9571ad8c13
Siddhartha Dhar Choudhury <38374913+frankhart2018@users.noreply.github.com>
2019-01-04 22:23:49 +0530

    Update itemPut.php

--- itemPut.php
+++ itemPut.php
@@ -1,6 +1,6 @@
 <?php
 
-    $link = mysqli_connect("shareddb-g.hosting.stackcp.net","vastukosh-32353f7f","password98@","vastukosh-32353f7f");
+    $link = mysqli_connect(*******);
 
     $words = Array('ability', 'able', 'about', 'above', 'behavior', 'behind', 'believe', 'benefit', 'card', 'care', 'career', 'carry', 'democratic', 'describe', 'design', 'despite', 'eight', 'either', 'election', 'else', 'fear', 'federal', 'feel', 'feeling', 'goal', 'good', 'government', 'great', 'herself', 'high', 'him', 'himself', 'including', 'increase', 'indeed', 'indicate', 'job', 'join', 'just', 'juggle', 'kind', 'kitchen', 'know', 'knowledge', 'lead', 'leader', 'learn', 'least', 'maintain', 'major', 'majority', 'make', 'national', 'natural', 'nature', 'near', 'open', 'operation', 'opportunity', 'option', 'particularly', 'partner', 'party', 'pass', 'quality', 'question', 'quickly', 'quite', 'reach', 'read', 'ready', 'real', 'scientist', 'score', 'sea', 'season', 'third', 'this', 'those', 'though', 'understand', 'unit', 'until', 'up', 'victim', 'view', 'violence', 'visit', 'want', 'war', 'watch', 'water', 'yeah', 'year', 'yes', 'yet');
 
